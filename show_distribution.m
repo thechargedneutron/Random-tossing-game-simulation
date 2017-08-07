893731d2b85c75436412bbf38ab54500acbf85da
kumarashutosh.ee@gmail.com <kumarashutosh.ee@gmail.com>
2017-08-08 01:22:50 +0530

    First commit

---
+++ show_distribution.m
@@ -0,0 +1,14 @@
+function [] = show_distribution(number_of_people, number_of_iterations, no_of_bins)
+data = zeros(number_of_people,2);
+data(:,1) = 1:number_of_people; data(:,2) = 50;
+
+for _ = 1:number_of_iterations
+r = randi([1,number_of_people],1,2);
+won = randi([1,2],1,1);
+data(r(1,won),2) = data(r(1,won),2) +1;
+data(r(1,3-won),2) = data(r(1,3-won),2) +1;
+endfor
+freq = data(:,2);
+histogram = hist(freq,no_of_bins);
+plot(histogram);
+end
